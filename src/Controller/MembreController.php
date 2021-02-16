5bf255ea25cc7f468be6c7bb51e6e3e838cc5168
Doriane P <dorianepinet@hotmail.com>
2021-02-16 14:46:39 +0100

    mise à jour

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -15,4 +15,38 @@ class MembreController extends AbstractController
             'controller_name' => 'MembreController',
         ]);
     }
+
+
+
+    // #[Route('/annonce/new', name: 'annonce_new', methods: ['GET', 'POST'])]
+    // public function new(Request $request): Response
+    // {
+    //     $annonce = new Annonce();
+    //     $form = $this->createForm(AnnonceType::class, $annonce);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+
+    //         // Completer les infos manquantes
+    //         $annonce->setDatePublication(new \DateTime());
+    //         // https://symfony.com/doc/current/security.html#a-fetching-the-user-object
+    //         // Ajouter l'auteur de l'annonce avec l'utilisateur connecté
+    //         $userConnecte = $this->getUser();
+    //         $annonce->setUser($userConnecte);
+
+    //         // Code qui insere la nouvelle ligne dans SQL
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($annonce);
+    //         $entityManager->flush();
+
+    //         return $this->redirectToRoute('annonce_index');
+    //     }
+
+    //     return $this->render('annonce/new.html.twig', [
+    //         'annonce' => $annonce,
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
+
 }
+
