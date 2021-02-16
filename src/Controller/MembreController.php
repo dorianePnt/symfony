a824a80a44cc4fa0f64a790ba1317088bd64ab01
Doriane P <dorianepinet@hotmail.com>
2021-02-16 16:06:10 +0100

    ajout formulaire creation annonce dans espace membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -6,47 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Annonce;
+use App\Form\AnnonceType;
+
+
+
 class MembreController extends AbstractController
 {
-    #[Route('/membre', name: 'membre')]
-    public function index(): Response
+    #[Route('/membre', name: 'membre', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        $annonce = new Annonce();
+        $form = $this->createForm(AnnonceType::class, $annonce);
+        $form->handleRequest($request);
+
+        $messageConfirmation = "";
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Completer les infos manquantes
+            $annonce->setDatePublication(new \DateTime());
+            // https://symfony.com/doc/current/security.html#a-fetching-the-user-object
+            // Ajouter l'auteur de l'annonce avec l'utilisateur connecté
+            $userConnecte = $this->getUser();
+            $annonce->setUser($userConnecte);
+
+            // Code qui insere la nouvelle ligne dans SQL
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($annonce);
+            $entityManager->flush();
+
+            $messageConfirmation = "Votre annonce est publiée";
+        }
+
         return $this->render('membre/index.html.twig', [
-            'controller_name' => 'MembreController',
+            'annonce' => $annonce,
+            'form' => $form->createView(),
+            'messageConfirmation' => $messageConfirmation
         ]);
     }
-
-
-
-    // #[Route('/annonce/new', name: 'annonce_new', methods: ['GET', 'POST'])]
-    // public function new(Request $request): Response
-    // {
-    //     $annonce = new Annonce();
-    //     $form = $this->createForm(AnnonceType::class, $annonce);
-    //     $form->handleRequest($request);
-
-    //     if ($form->isSubmitted() && $form->isValid()) {
-
-    //         // Completer les infos manquantes
-    //         $annonce->setDatePublication(new \DateTime());
-    //         // https://symfony.com/doc/current/security.html#a-fetching-the-user-object
-    //         // Ajouter l'auteur de l'annonce avec l'utilisateur connecté
-    //         $userConnecte = $this->getUser();
-    //         $annonce->setUser($userConnecte);
-
-    //         // Code qui insere la nouvelle ligne dans SQL
-    //         $entityManager = $this->getDoctrine()->getManager();
-    //         $entityManager->persist($annonce);
-    //         $entityManager->flush();
-
-    //         return $this->redirectToRoute('annonce_index');
-    //     }
-
-    //     return $this->render('annonce/new.html.twig', [
-    //         'annonce' => $annonce,
-    //         'form' => $form->createView(),
-    //     ]);
-    // }
 
 }
 
