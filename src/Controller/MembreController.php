2019f5c40848bc9949425b5452a20f9f01ebfcf6
Doriane P <dorianepinet@hotmail.com>
2021-02-17 12:01:08 +0100

    affichage des annonces du membre connecté

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -9,26 +9,30 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Annonce;
 use App\Form\AnnonceType;
+use App\Repository\AnnonceRepository;
 
 
 
 class MembreController extends AbstractController
 {
     #[Route('/membre', name: 'membre', methods: ['GET', 'POST'])]
-    public function index(Request $request): Response
+    public function index(Request $request, AnnonceRepository $annonceRepository): Response
     {
         $annonce = new Annonce();
         $form = $this->createForm(AnnonceType::class, $annonce);
         $form->handleRequest($request);
 
         $messageConfirmation = "";
+
+        // Ajouter l'auteur de l'annonce avec l'utilisateur connecté
+        $userConnecte = $this->getUser();
+
         if ($form->isSubmitted() && $form->isValid()) {
 
             // Completer les infos manquantes
             $annonce->setDatePublication(new \DateTime());
             // https://symfony.com/doc/current/security.html#a-fetching-the-user-object
-            // Ajouter l'auteur de l'annonce avec l'utilisateur connecté
-            $userConnecte = $this->getUser();
+            
             $annonce->setUser($userConnecte);
 
             // Code qui insere la nouvelle ligne dans SQL
@@ -39,8 +43,16 @@ class MembreController extends AbstractController
             $messageConfirmation = "Votre annonce est publiée";
         }
 
+        // après le traitement du create pour obtenir la liste à jour
+        $annonces = $annonceRepository->findBy([
+            "user" => $userConnecte,            
+            // on filtre les lignes pour obtenir seulement les annonces de l'utilisateur
+        ], [ "datePublication" => "DESC"]);
+        
+
         return $this->render('membre/index.html.twig', [
             'annonce' => $annonce,
+            'annonces' => $annonces,    // SELECT * FROM annonces
             'form' => $form->createView(),
             'messageConfirmation' => $messageConfirmation
         ]);
